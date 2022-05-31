2474e7c566e7662a417a4f5cf6b923f3f2e62276
Booma Shree <768656@cognizant.com>
2022-06-01 04:32:31 +0530

    ini

---
+++ pensioner-detail-microservice/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+ INSERT into Pensioner_Detail (AADHAAR_NUMBER,	NAME,DATE_OF_BIRTH,	PAN_NUMBER,	SALARY,	ALLOWANCE,	PENSION_TYPE,	BANK_NAME,	ACCOUNT_NUMBER,	BANK_TYPE ) 
+ VALUES
+    ('123456789012',	'Iftak',	'29/01/99',	'PCASD1234Q',	'27000',	'10000',	'self',	'ICICI',	'12345678',	'private'),
+    ('123456789013',	'Pratyush', '27/11/98',	'PCQAZ1285Q',	'30000',	'12000',	'family',	'SBI',	'12345679',	'public'),
+    ('123456789014',	'Sanyam',	'24/10/98',	'PCERF1212E',	'30001',	'12001',	'self',	'HDFC',	'12345680',	'private'),
+    ('123456789015',	'Surya',	'02/07/97',	'PCSDZ1287G',	'40000',	'12002',	'family',	'SBI',	'12345681',	'public'),
+    ('123456789077',	'Booma',	'12/09/56',	'GHFKJ67894',	'78000',	'9000',	'family',	'ICIC',	'12345682',	'Private');
